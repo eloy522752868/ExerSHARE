b4b31b2d28a6d72ac9d943e42006594138661594
Matt Hale <mhale@factharbor.com>
2021-04-28 14:31:40 -0400

    updated home handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,13 @@
-const router = require('express').Router();
-const {User} = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
-
-router.get('/', async (req, res) => {
-    try {
-        res.send('Welcome to ExerShare');
-        console.log("access homepage from browser not api controller get/")
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
